6b136e7c880e0f72e69c6c195f29f45b448ff089
irynasunshine <irina060822@gmail.com>
2026-06-12 20:55:43 +0300

    add entrypoint file to start an app

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.cli.commands import app  # Import the CLI src object from the commands module
+
+if __name__ == "__main__":  # Run only when this file is executed directly (not imported)
+    app()  # Start the CLI application
